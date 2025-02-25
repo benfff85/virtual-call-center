d0e503472cefa59952cb88769da31f285e45a49e
Ben Ferenchak <ben.ferenchak@gmail.com>
2025-02-24 21:36:11 -0500

    Made single vs multi agent config driven by env

--- services/conversation_segment_processor_service.py
+++ services/conversation_segment_processor_service.py
@@ -1,10 +1,10 @@
 import logging
+import os
 from typing import Optional
 
 from clients.twilio_rest_client import interrupt_specialist_audio
 from schemas.conversation_output_channel_type import ConversationOutputChannelType
 from schemas.conversation_segment import ConversationSegment
-from services.agents.agentic_service_complex_team import AgenticService
 from services.audio_persistence_service import AudioPersistenceService
 from services.conversation_channels.output.console_output_channel_service import ConsoleOutputChannelService
 from services.conversation_channels.output.twilio_output_channel_service import TwilioOutputChannelService
@@ -22,18 +22,23 @@ class ConversationSegmentProcessorService:
     def __init__(
             self,
             transcription_gateway: Optional[TranscriptionGateway] = TranscriptionGateway(),
-            agentic_service: Optional[AgenticService] = AgenticService(),
             tts_service: Optional[KokoroTtsService] = KokoroTtsService(),
             audio_persistence_service: Optional[AudioPersistenceService] = AudioPersistenceService(),
             logger: Optional[logging.Logger] = configure_logger('conversation_segment_processor_service_logger', logging.INFO)
     ):
 
         self.transcription_gateway = transcription_gateway or TranscriptionGateway()
-        self.agentic_service = agentic_service or AgenticService()
         self.tts_service = tts_service or KokoroTtsService()
         self.audio_persistence_service = audio_persistence_service or AudioPersistenceService()
         self.audio_output_channel = TwilioOutputChannelService() # TODO Consider adding gateway to drive off of convo segment output channel type
         self.console_output_channel = ConsoleOutputChannelService()
+
+        if os.environ.get('AGENT_TYPE') == 'simple':
+            from services.agents.agentic_service import AgenticService
+        else:
+            from services.agents.agentic_service_complex_team import AgenticService
+        self.agentic_service = AgenticService()
+
         self.logger = logger
 
     async def process_conversation_segment(self, conversation_segment: ConversationSegment):
